044c3689ec08fb4763a326d946f6e81f62aba0e1
unknown <aghazjunaid96@gmail.com>
2022-03-02 15:03:11 +0530

    misc upd

---
+++ Programming Practice/pattern.js
@@ -0,0 +1,13 @@
+// this is test
+// tit
+// hse
+// i s
+// s t
+
+let str = "this is test";
+let splitData = str.split(" ") //[ 'this', 'is', 'test' ]
+
+let newStr = "";
+for(let i=0;i<splitData.length;i++){
+    
+}
