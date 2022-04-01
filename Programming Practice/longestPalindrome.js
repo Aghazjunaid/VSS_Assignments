27d7ca2764ca1746c6eeb867242cf1a2c3b074b2
unknown <aghazjunaid96@gmail.com>
2022-04-01 10:37:28 +0530

    find substring using loop

---
+++ Programming Practice/longestPalindrome.js
@@ -0,0 +1,9 @@
+// // find substring
+// let str="naz"
+// let arr = []
+// for(let i=0; i<str.length;i++){
+//     for(let j=i; j<str.length;j++){
+//         arr.push(str.slice(i,j+1))
+//     }
+// }
+// console.log(arr) //[ 'n', 'na', 'naz', 'a', 'az', 'z' ]
---
+++ Programming Practice/test.js
@@ -0,0 +1,8 @@
+let str="naz"
+let arr = []
+for(let i=0; i<str.length;i++){
+    for(let j=i; j<str.length;j++){
+        arr.push(str.slice(i,j+1))
+    }
+}
+console.log(arr) //[ 'n', 'na', 'naz', 'a', 'az', 'z' ]
