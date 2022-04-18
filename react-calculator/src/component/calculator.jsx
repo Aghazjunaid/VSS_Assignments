d0b8932b0e3e5f381778bba3b271a15274d40bff
unknown <aghazjunaid96@gmail.com>
2022-04-18 13:09:38 +0530

    calculator app done

--- react-calculator/src/component/calculator.jsx
+++ react-calculator/src/component/calculator.jsx
@@ -6,7 +6,8 @@ class Calculator extends Component{
         this.state = {
             num1: 0,
             num2: 0,
-            operation: ''
+            operation: '',
+            total: ''
         }
     }
 
@@ -33,8 +34,41 @@ class Calculator extends Component{
         })
     }
 
-    calculate = () => {
-
+    calculate = (e) => {
+        e.preventDefault();
+        if(this.state.operation === "add"){
+            let total = Number(this.state.num1) +  Number(this.state.num2)
+            this.setState({
+                ...this.state,
+                total : total
+            })
+        }else if(this.state.operation === "substract"){
+            let total = Number(this.state.num1) -  Number(this.state.num2)
+            this.setState({
+                ...this.state,
+                total : total
+            })
+        }else if(this.state.operation === "multiply"){
+            let total = Number(this.state.num1) *  Number(this.state.num2)
+            this.setState({
+                ...this.state,
+                total : total
+            })
+        }else if(this.state.operation === "divide"){
+            let total = Number(this.state.num1) /  Number(this.state.num2)
+            this.setState({
+                ...this.state,
+                total : total
+            })
+        }else if(this.state.operation === "remainder"){
+            let total = Number(this.state.num1) %  Number(this.state.num2)
+            this.setState({
+                ...this.state,
+                total : total
+            })
+        }else{
+            alert("Invalid Operation")
+        }
     }
 
 
@@ -43,23 +77,27 @@ class Calculator extends Component{
             <>
                 <form>
                     <div>
-                        <input type={"number"} placeholder="Enter first number" onChange={this.setFirstNumber}/>
+                        <input type={"number"} placeholder="Enter first number" onChange={this.setFirstNumber} required/>
                     </div>
                     <div>
-                        <input type={"number"} placeholder="Enter second number" onChange={this.setSecondNumber}/>
+                        <input type={"number"} placeholder="Enter second number" onChange={this.setSecondNumber} required/>
                     </div>
-                    <select name="operation" onChange={this.setOperation}>
+                    <select name="operation" onChange={this.setOperation} required>
                         <option>Select operation</option>
                         <option value="add">Add</option>
                         <option value="substract">Substract</option>
                         <option value="multiply">Multiply</option>
                         <option value="divide">Divide</option>
+                        <option value="remainder">Find Remainder</option>
                     </select>
                     <div>
                         <button type='submit' onClick={this.calculate}>Calculate</button>
                     </div>
                 </form>
-            
+                <br/>
+                <br/>
+                <br/>
+                <h3>Your Output is : {this.state.total}</h3>
             </>
         )
     }
